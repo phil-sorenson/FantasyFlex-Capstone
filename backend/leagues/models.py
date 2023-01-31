6dcba33cebab03a67ce872d3c79b7a5237c40cd8
phil-sorenson <phillip.sorenson31@gmail.com>
2023-01-31 00:15:17 -0600

    Switched up the leaues serializers by adding a viewset + Commented out leagues views + got rid of model is_imported

--- backend/leagues/models.py
+++ backend/leagues/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from authentication.models import User
-from fantasy_platforms.models import Platform
+# from fantasy_platforms.models import Platform
 # Create your models here.
 
 class League(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    platform = models.ForeignKey(Platform, on_delete=models.CASCADE)
+    platform = models.CharField(max_length=255)
     league_name = models.CharField(max_length=255)
-    league_id = models.CharField(max_length=255)
+    league_id = models.IntegerField(max_length=255, unique=True)
     # is_imported tracks whether league was imported to user's account or not
     is_imported = models.BooleanField(default=False)
 
--- backend/leagues/serializers.py
+++ backend/leagues/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers
+from rest_framework import serializers, viewsets
 from .models import League
 
 
@@ -8,7 +8,10 @@ class LeagueSerializer(serializers.ModelSerializer):
         fields = ['id', 'user' 'platform', 'league_name', 'league_id']
         #?❓Would 'platform' need to be in here since the platform will already be chosen by user once he is told to pick through his leagues and we have a fantasy_platforms app already
 
-class GetLeaguesSerializer(serializers.Serializer):
-#  This serializer class will validate the data coming in from the user's selected leagues API call
-    platform_id = serializers.IntegerField()
-    selected_leagues = serializers.ListField(child = serializers.IntegerField())
+class LeagueViewSet(viewsets.ModelViewSet):
+    queryset = League.objects.all()
+    serializer_class = LeagueSerializer
+# class GetLeaguesSerializer(serializers.Serializer):
+# #  This serializer class will validate the data coming in from the user's selected leagues API call
+#     platform_id = serializers.IntegerField()
+#     selected_leagues = serializers.ListField(child = serializers.IntegerField())
